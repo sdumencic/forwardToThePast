6de77f7244dee4fe7de00c15dceb843505af7bc9
stelladum <stella.dumencic@gmail.com>
2021-04-23 17:07:17 +0200

    Close #19

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityMenu();
+            }
+        });
+    }
+
+    public void openActivityMenu() {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
     }
 }
