70f27f7b8b3d9a5deff4d849152018181f3ed3d9
stelladum <stella.dumencic@gmail.com>
2021-05-25 11:32:16 +0200

    gui testing

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -2,12 +2,21 @@ package com.example.myapplication;
 
 import android.content.Context;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +26,21 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityTestRule = new ActivityScenarioRule<> (MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.myapplication", appContext.getPackageName());
+    }
+
+    @Test
+    public void clickPlay() throws Exception {
+        /*onView(withId(R.id.button2)).perform(click());
+        Espresso.pressBack();*/
+        //onView(withId(R.id.snake)).check(matches(isDisplayed()));
+        onView(withText("Play")).perform(click());
     }
 }
